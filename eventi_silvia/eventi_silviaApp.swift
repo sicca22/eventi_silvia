daf55d209a82f156fda8e18e63bbac28e3ea24c2
Silvia Cicala <silviacicala@outlook.it>
2022-05-10 16:32:06 +0200

    Initial Commit

---
+++ eventi_silvia/eventi_silviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  eventi_silviaApp.swift
+//  eventi_silvia
+//
+//  Created by iedstudent on 10/05/22.
+//
+
+import SwiftUI
+
+@main
+struct eventi_silviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
